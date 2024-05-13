189ec217b42e3b329416126cb5c84ed98dd9d7ac
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-05-13 12:44:54 +0900

    Add Javascript Appium test

---
+++ src/tests/test.js
@@ -0,0 +1,43 @@
+const {remote} = require('webdriverio');
+
+const capabilities = {
+  platformName: 'Android',
+  'appium:automationName': 'UiAutomator2',
+  'appium:deviceName': 'emulator-5554',
+  'appium:appPackage': 'com.example.framework7',
+  'appium:appActivity': '.MainActivity',
+};
+
+const wdOpts = {
+  hostname: process.env.APPIUM_HOST || 'localhost',
+  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+  logLevel: 'info',
+  capabilities,
+};
+
+async function runTest() {
+  const driver = await remote(wdOpts);
+  try {
+    const input = await driver.$('//android.widget.EditText');
+    await input.waitForExist(600000);  // wait up to 10 mins
+    await input.setValue('What is the capital of Japan?');
+    const link = await driver.$('//android.widget.TextView[@text="Send"]');
+    await link.waitForExist(600000);  // wait up to 10 mins
+    await link.click();
+    const textElement = await driver.$('//android.widget.TextView[@text="Tokyo"]');
+    await textElement.waitForExist(600000);  // wait up to 10 mins
+    const text = await textElement.getText();
+
+    if (text === 'Tokyo') {
+      console.log('Text "Tokyo" found');
+    } else {
+      console.log('Text "Tokyo" not found');
+    }
+
+  } finally {
+    await driver.pause(3000);
+    await driver.deleteSession();
+  }
+}
+
+runTest().catch(console.error);
